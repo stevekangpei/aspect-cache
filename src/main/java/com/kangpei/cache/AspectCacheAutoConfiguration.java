68b88cf8758622ae42f8ac14d6e4f8ddad6ba6dc
stevekangpei <stevekangp@outlook.com>
2020-11-07 19:24:57 +0800

    首次提交

---
+++ src/main/java/com/kangpei/cache/AspectCacheAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.kangpei.cache;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * description: AspectCacheAutoConfiguration <br>
+ * date: 2020/10/29 9:49 下午 <br>
+ * author: kangpei <br>
+ * version: 1.0 <br>
+ */
+@ComponentScan("")
+@EnableAspectJAutoProxy
+public class AspectCacheAutoConfiguration {
+}
